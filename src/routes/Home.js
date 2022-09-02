f75afed1b66f55f93f4247148dcaf4afe3127ddd
GraphTree <graphnodesed@gmail.com>
2022-09-02 09:49:12 +0900

    created grid styled todo list box

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,23 +1,49 @@
+import styled from "styled-components";
+
+
+    
+
+const GirdContainer= styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, minmax(0, 1fr));
+
+
+`
+
+const GirdItem = styled.div`
+    text-align:center;
+    background-color:lightblue;
+    border: 1px solid black;
+    margin: 10px
+`
+
+const TitleBox = styled.div`
+text-align : left;
+word-wrap: break-word
+`
+
+const ContentBox = styled(TitleBox)`
+    height : 200px;
+`
 
 
 function ToDO (){
     return(
-        <div>
+        <GirdItem>
             <h3> Title </h3>
-            <span>title is</span>
+            <TitleBox>title is</TitleBox>
             <h3>Content</h3>
-            <span>content is</span>
-        </div>
+            <ContentBox>content</ContentBox>
+        </GirdItem>
     )
 }
-    
-
 
 function Home() {
   return (
     <div>
-      <h1>Home</h1>
-      <ToDO/>
+        <GirdContainer>
+            <ToDO/>
+        </GirdContainer>
     </div>
   );
 }
